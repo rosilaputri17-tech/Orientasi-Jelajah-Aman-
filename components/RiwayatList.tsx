4127c41c89d1530c9fb5a31630a2c897fcfd3682
Rosila Agustina Putri <rosila.agustina.putri24029@mhs.uingusdur.ac.id>
2026-09-14 12:40:11 +0700

    feat: buat komponen RiwayatList

---
+++ components/RiwayatList.tsx
@@ -0,0 +1,16 @@
+// components/RiwayatList.tsx
+import { View, Text } from "react-native";
+
+interface RiwayatListProps {
+daftarKota: string[];
+}
+
+export default function RiwayatList({ daftarKota }: RiwayatListProps) {
+    return (
+    <View>
+        {daftarKota.map((kota) => (
+            <Text key={kota}>{kota}</Text>
+            ))}
+            </View>
+            );
+}
