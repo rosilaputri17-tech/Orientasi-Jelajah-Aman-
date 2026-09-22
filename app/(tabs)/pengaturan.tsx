36ad9c8d22c42c38a2559a49c167ca790b81c272
Rosila Agustina Putri <rosila.agustina.putri24029@mhs.uingusdur.ac.id>
2026-09-22 07:15:48 +0700

    feat: update halaman pengaturan dan riwayat

--- app/(tabs)/pengaturan.tsx
+++ app/(tabs)/pengaturan.tsx
@@ -1,10 +1,15 @@
 // app/(tabs)/pengaturan.tsx
-import { View, Text } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 export default function TabPengaturan() {
-return (
-<View style={{ padding: 16 }}>
-<Text style={{ fontSize: 18, fontWeight: "bold" }}>Jelajah Aman</Text>
-<Text>Versi 1.0.0</Text>
-</View>
-);
+  return (
+    <SafeAreaView style={{ flex: 1, padding: 16 }}>
+      <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+        Jelajah Aman
+      </Text>
+
+      <Text>Versi 1.0.0</Text>
+    </SafeAreaView>
+  );
 }
--- app/(tabs)/riwayat.tsx
+++ app/(tabs)/riwayat.tsx
@@ -1,11 +1,10 @@
-// app/(tabs)/riwayat.tsx
-import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import RiwayatList from "../../components/RiwayatList";
 export default function TabRiwayat() {
-const daftarKota = ["Pekalongan", "Jakarta", "Semarang"]; // data contoh sementara
+const daftarKota = ["Pekalongan", "Jakarta", "Semarang"];
 return (
-<View style={{ padding: 16 }}>
+<SafeAreaView style={{ flex: 1, padding: 16 }}>
 <RiwayatList daftarKota={daftarKota} />
-</View>
+</SafeAreaView>
 );
 }
