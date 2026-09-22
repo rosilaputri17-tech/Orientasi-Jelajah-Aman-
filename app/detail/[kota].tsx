ba8bf9086a1570a137b931792895ee995b1d12a5
Rosila Agustina Putri <rosila.agustina.putri24029@mhs.uingusdur.ac.id>
2026-09-22 13:30:40 +0700

    update [kota].tsx

--- app/detail/[kota].tsx
+++ app/detail/[kota].tsx
@@ -1,12 +1,16 @@
-// app/detail/[kota].tsx
-import { View } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+// src/app/detail/[kota].tsx
+import { View, Button } from "react-native";
+import { useLocalSearchParams, router } from "expo-router";
 import WeatherCard from "../../components/WeatherCard";
 export default function HalamanDetail() {
 const { kota } = useLocalSearchParams<{ kota: string }>();
 return (
-<View style={{ padding: 16 }}>
+<View style={{ padding: 16, gap: 16 }}>
 <WeatherCard kota={kota} suhu={29} tingkatAQI="BAIK" />
+<Button
+title="Tambahkan ke Favorit"
+onPress={() => router.push("/tambah-favorit")}
+/>
 </View>
 );
 }
